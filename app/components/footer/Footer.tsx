080e2f1f33bb815869b4df502e7d315ba3139f0e
Aleksandar Timic <aleksandar.timic@meld.com>
2023-12-28 15:45:19 +0100

    Footer top update

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -3,8 +3,11 @@
 import styles from "./Footer.module.scss";
 import ArrowUp from "./assets/arrow-up.png";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 export default function Footer() {
+  const pathname = usePathname();
+
   const handleBackToTop = () => {
     window.scrollTo({
       top: 0,
@@ -12,24 +15,31 @@ export default function Footer() {
       behavior: "smooth",
     });
   };
+
+  const showFooterTop = !["/contact"].includes(pathname);
+
   return (
     <div className={styles.footer}>
       <div className={styles.footerInner}>
-        <div className={styles.footerLeft}>
-          <p>Interested in working together?</p>
-          <p>
-            Reach out <a href={"/contacts"}>here</a> or stay in touch via{" "}
-            <a
-              href={"https://www.linkedin.com/in/kieran-hurley-44919b5a/"}
-              target={"_blank"}
-            >
-              LinkedIn
-            </a>
-          </p>
-        </div>
-        <div className={styles.backToTop} onClick={handleBackToTop}>
-          <Image src={ArrowUp} alt={"Back to top"} />
-        </div>
+        {showFooterTop && (
+          <div className={styles.footerTop}>
+            <div className={styles.footerLeft}>
+              <p>Interested in working together?</p>
+              <p>
+                Reach out <a href={"/contacts"}>here</a> or stay in touch via{" "}
+                <a
+                  href={"https://www.linkedin.com/in/kieran-hurley-44919b5a/"}
+                  target={"_blank"}
+                >
+                  LinkedIn
+                </a>
+              </p>
+            </div>
+            <div className={styles.backToTop} onClick={handleBackToTop}>
+              <Image src={ArrowUp} alt={"Back to top"} />
+            </div>
+          </div>
+        )}
       </div>
       <div className={styles.copyright}>
         <p>© All rights reserved. Kieran Hurley 2023</p>
