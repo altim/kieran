7920b97bead336305b8fc7e3044b42612f637848
Aleksandar Timic <aleksandar.timic@meld.com>
2024-01-05 20:47:42 +0100

    Update

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,18 +1,20 @@
 import Link from "next/link";
 import styles from "./page.module.scss";
-
+import { projects } from "@/data/data.ts";
 export default function PortfolioHub() {
+  const categories = projects
+    .map((item) => item.category)
+    .filter((item2, i, cats) => cats.indexOf(item2) === i);
+
   return (
     <div className={styles.portfolioHub}>
       <div className={styles.portfolioHubInner}>
-        <Link href={"/portfolio/featured"}>Featured Work</Link>
-        <br />
-        <Link href={"/portfolio/cat1"}>Category2</Link>
-        <br />
-        <Link href={"/portfolio/cat2"}>Category3</Link>
-        <br />
-        <Link href={"/portfolio/cat3"}>Category4</Link>
-        <br />
+        {categories.map((cat) => (
+          <>
+            <Link href={`/portfolio/${cat}`}>{cat}</Link>
+            <br />
+          </>
+        ))}
       </div>
     </div>
   );
--- data/data.ts
+++ data/data.ts
@@ -168,7 +168,111 @@ export const projects: ProjectType[] = [
 
     title: "GoPro 4",
     subtitle: "OOH / Experiential",
-    category: "featured",
+    category: "cat1",
+    paragraphs: [
+      {
+        title: "Problem",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat faucibus hendrerit. Quisque a tellus sollicitudin quam congue volutpat a non nulla. Aenean sollicitudin malesuada lacus, sit amet vestibulum ipsum condimentum vel. Curabitur vitae nunc accumsan, euismod justo vel, ornare dolor. Cras nec orci sapien. Ut ornare et est et aliquam. Nulla at hendrerit velit. Donec sed interdum augue. Suspendisse turpis orci, pretium vel porta quis, vehicula ac erat. Etiam feugiat ac sem eget fermentum. Mauris eget augue at enim condimentum pellentesque. Integer luctus, lectus id dignissim lobortis, risus quam sodales ex, vel cursus ante massa quis massa. Vestibulum ultricies aliquet purus, nec rutrum arcu scelerisque nec. Phasellus vestibulum est a consequat rhoncus.",
+      },
+      {
+        title: "Solution",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat faucibus hendrerit. Quisque a tellus sollicitudin quam congue volutpat a non nulla. Aenean sollicitudin malesuada lacus, sit amet vestibulum ipsum condimentum vel. Curabitur vitae nunc accumsan, euismod justo vel, ornare dolor. Cras nec orci sapien. Ut ornare et est et aliquam. Nulla at hendrerit velit. Donec sed interdum augue. Suspendisse turpis orci, pretium vel porta quis, vehicula ac erat. Etiam feugiat ac sem eget fermentum. Mauris eget augue at enim condimentum pellentesque. Integer luctus, lectus id dignissim lobortis, risus quam sodales ex, vel cursus ante massa quis massa. Vestibulum ultricies aliquet purus, nec rutrum arcu scelerisque nec. Phasellus vestibulum est a consequat rhoncus.",
+      },
+      {
+        title: "My Role",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat faucibus hendrerit. Quisque a tellus sollicitudin quam congue volutpat a non nulla. Aenean sollicitudin malesuada lacus, sit amet vestibulum ipsum condimentum vel. Curabitur vitae nunc accumsan, euismod justo vel, ornare dolor. Cras nec orci sapien. Ut ornare et est et aliquam. Nulla at hendrerit velit. Donec sed interdum augue. Suspendisse turpis orci, pretium vel porta quis, vehicula ac erat. Etiam feugiat ac sem eget fermentum. Mauris eget augue at enim condimentum pellentesque. Integer luctus, lectus id dignissim lobortis, risus quam sodales ex, vel cursus ante massa quis massa. Vestibulum ultricies aliquet purus, nec rutrum arcu scelerisque nec. Phasellus vestibulum est a consequat rhoncus.",
+      },
+    ],
+    images: [
+      {
+        src: "/assets/portfolio/portfolio-item/portfolio-item-image1.jpg",
+        alt: "Portfolio Item Image",
+        width: 448,
+        height: 448,
+        size: "normal",
+      },
+      {
+        src: "/assets/portfolio/portfolio-item/portfolio-item-image1.jpg",
+        alt: "Portfolio Item Image",
+        width: 448,
+        height: 448,
+        size: "normal",
+      },
+      {
+        src: "/assets/portfolio/portfolio-item/portfolio-item-image2.jpg",
+        alt: "Portfolio Item Image",
+        width: 920,
+        height: 448,
+        size: "fullWidth",
+      },
+    ],
+    relatedProjects: ["gopro", "gopro2"],
+  },
+
+  {
+    slug: "gopro5",
+    featuredImage: {
+      src: "/assets/portfolio/portfolio-item/featured-1.jpg",
+      alt: "Featured Image",
+      width: 920,
+      height: 448,
+    },
+
+    title: "GoPro 5",
+    subtitle: "OOH / Experiential",
+    category: "cat2",
+    paragraphs: [
+      {
+        title: "Problem",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat faucibus hendrerit. Quisque a tellus sollicitudin quam congue volutpat a non nulla. Aenean sollicitudin malesuada lacus, sit amet vestibulum ipsum condimentum vel. Curabitur vitae nunc accumsan, euismod justo vel, ornare dolor. Cras nec orci sapien. Ut ornare et est et aliquam. Nulla at hendrerit velit. Donec sed interdum augue. Suspendisse turpis orci, pretium vel porta quis, vehicula ac erat. Etiam feugiat ac sem eget fermentum. Mauris eget augue at enim condimentum pellentesque. Integer luctus, lectus id dignissim lobortis, risus quam sodales ex, vel cursus ante massa quis massa. Vestibulum ultricies aliquet purus, nec rutrum arcu scelerisque nec. Phasellus vestibulum est a consequat rhoncus.",
+      },
+      {
+        title: "Solution",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat faucibus hendrerit. Quisque a tellus sollicitudin quam congue volutpat a non nulla. Aenean sollicitudin malesuada lacus, sit amet vestibulum ipsum condimentum vel. Curabitur vitae nunc accumsan, euismod justo vel, ornare dolor. Cras nec orci sapien. Ut ornare et est et aliquam. Nulla at hendrerit velit. Donec sed interdum augue. Suspendisse turpis orci, pretium vel porta quis, vehicula ac erat. Etiam feugiat ac sem eget fermentum. Mauris eget augue at enim condimentum pellentesque. Integer luctus, lectus id dignissim lobortis, risus quam sodales ex, vel cursus ante massa quis massa. Vestibulum ultricies aliquet purus, nec rutrum arcu scelerisque nec. Phasellus vestibulum est a consequat rhoncus.",
+      },
+      {
+        title: "My Role",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consequat faucibus hendrerit. Quisque a tellus sollicitudin quam congue volutpat a non nulla. Aenean sollicitudin malesuada lacus, sit amet vestibulum ipsum condimentum vel. Curabitur vitae nunc accumsan, euismod justo vel, ornare dolor. Cras nec orci sapien. Ut ornare et est et aliquam. Nulla at hendrerit velit. Donec sed interdum augue. Suspendisse turpis orci, pretium vel porta quis, vehicula ac erat. Etiam feugiat ac sem eget fermentum. Mauris eget augue at enim condimentum pellentesque. Integer luctus, lectus id dignissim lobortis, risus quam sodales ex, vel cursus ante massa quis massa. Vestibulum ultricies aliquet purus, nec rutrum arcu scelerisque nec. Phasellus vestibulum est a consequat rhoncus.",
+      },
+    ],
+    images: [
+      {
+        src: "/assets/portfolio/portfolio-item/portfolio-item-image1.jpg",
+        alt: "Portfolio Item Image",
+        width: 448,
+        height: 448,
+        size: "normal",
+      },
+      {
+        src: "/assets/portfolio/portfolio-item/portfolio-item-image1.jpg",
+        alt: "Portfolio Item Image",
+        width: 448,
+        height: 448,
+        size: "normal",
+      },
+      {
+        src: "/assets/portfolio/portfolio-item/portfolio-item-image2.jpg",
+        alt: "Portfolio Item Image",
+        width: 920,
+        height: 448,
+        size: "fullWidth",
+      },
+    ],
+    relatedProjects: ["gopro", "gopro2"],
+  },
+
+  {
+    slug: "gopro6",
+    featuredImage: {
+      src: "/assets/portfolio/portfolio-item/featured-1.jpg",
+      alt: "Featured Image",
+      width: 920,
+      height: 448,
+    },
+
+    title: "GoPro 6",
+    subtitle: "OOH / Experiential",
+    category: "cat3",
     paragraphs: [
       {
         title: "Problem",
