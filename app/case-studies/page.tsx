a35924ef2d8ce3c3949cd7112cd3de3b19bde8a2
Aleksandar Timic <aleksandar.timic@meld.com>
2024-01-14 12:50:03 +0100

    Page header

--- app/case-studies/page.tsx
+++ app/case-studies/page.tsx
@@ -1,19 +1,41 @@
 import styles from "./page.module.scss";
+import HeaderImage from "@/components/header-image/HeaderImage";
 import { caseStudies } from "@/data/data.ts";
-import CategoryLink from "@/components/category-link/CategoryLink.tsx";
+import CategoryItem from "@/components/category-item/CategoryItem.tsx";
 
-export default function CaseStudyHub() {
-  const categories = caseStudies
-    .map((item) => item.category)
-    .filter((item2, i, cats) => cats.indexOf(item2) === i);
-
+export default function PortfolioCategory() {
   return (
-    <div className={styles.caseStudyHub}>
-      <div className={styles.caseStudyHubInner}>
-        {categories.map((cat, index) => (
-          <CategoryLink category={cat} categoryType={"caseStudy"} key={index} />
+    <>
+      <HeaderImage
+        src={"/assets/case-studies/header.jpg"}
+        alt={"Case Studies"}
+        width={920}
+        height={448}
+        backgroundColor={"gray"}
+      />
+
+      <div className={styles.pageTitle}>
+        <div className={styles.pageTitleInner}>
+          <h1>Case Studies</h1>
+        </div>
+      </div>
+      <div className={styles.portfolioItemsList}>
+        {caseStudies.map((project, index) => (
+          <CategoryItem
+            imageSrc={project.excerpt.image.src}
+            imageAlt={project.excerpt.image.alt}
+            imageWidth={project.excerpt.image.width}
+            imageHeight={project.excerpt.image.height}
+            link={`/case-studies/${project.slug}`}
+            reverse={index % 2 === 1}
+            key={`portfolio-item-${index}`}
+          >
+            <h3>{project.excerpt.title}</h3>
+            <h3>{project.excerpt.subtitle}</h3>
+            <p>{project.excerpt.text}</p>
+          </CategoryItem>
         ))}
       </div>
-    </div>
+    </>
   );
 }
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,6 +1,7 @@
 import styles from "./page.module.scss";
 import { projects } from "@/data/data.ts";
 import CategoryLink from "@/components/category-link/CategoryLink.tsx";
+import HeaderImage from "@/components/header-image/HeaderImage.tsx";
 
 export default function PortfolioHub() {
   const categories = projects
@@ -8,12 +9,27 @@ export default function PortfolioHub() {
     .filter((item2, i, cats) => cats.indexOf(item2) === i);
 
   return (
-    <div className={styles.portfolioHub}>
-      <div className={styles.portfolioHubInner}>
-        {categories.map((cat, index) => (
-          <CategoryLink category={cat} categoryType={"project"} key={index} />
-        ))}
+    <>
+      <HeaderImage
+        src={"/assets/case-studies/header.jpg"}
+        alt={"Case Studies"}
+        width={920}
+        height={448}
+        backgroundColor={"gray"}
+      />
+
+      <div className={styles.pageTitle}>
+        <div className={styles.pageTitleInner}>
+          <h1>My Projects</h1>
+        </div>
       </div>
-    </div>
+      <div className={styles.portfolioHub}>
+        <div className={styles.portfolioHubInner}>
+          {categories.map((cat, index) => (
+            <CategoryLink category={cat} categoryType={"project"} key={index} />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
