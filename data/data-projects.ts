1490f5bbca33637bcdb66d8c6b2e726aab0dfc11
Aleksandar Timic <aleksandar.timic@meld.com>
2024-02-19 17:06:48 +0100

    Projects

--- data/data-projects.ts
+++ data/data-projects.ts
@@ -92,11 +92,135 @@ export const projects: ProjectType[] = [
     relatedProjects: [],
     excerpt: {
       title: "Campo Libre",
-      subtitle: "Munich Germany",
+      subtitle: "Munich, Germany",
       text: "“They’re on a mission to make better camping products”. Having immersed themselves in the Van Life movement for years, the founders of Campo Libre grew increasingly dissatisfied with the offerings in the camping market. The challenge was to create a camping brand target to a young adult  market with the mindset more akin to that of a skate, surf brand.",
       image: {
         src: "/assets/portfolio/campo-libre/campo-libre-1.jpg",
         alt: "Campo Libre",
+        width: 448,
+        height: 448,
+      },
+    },
+  },
+  {
+    slug: "gopro-creator-summit",
+    featuredImage: {
+      src: "/assets/portfolio/gp-slovenia/gp-slovenia-4.jpg",
+      alt: "GP Slovenia",
+      width: 920,
+      height: 448,
+    },
+
+    title: "GoPro Creator Summit",
+    subtitle: "Bled, Slovenia",
+    category: "branding",
+    paragraph:
+      "Following the launch of the HERO12 camera. GoPro invited a select group of athletes, advocates, and leading content creators to Bled, Slovenia. The purpose of the summit was to give hands-on product training and take part in some extreme activities in one of the world’s most spectacular playgrounds.",
+    details: {
+      date: "",
+      location: "Bled, Slovenia",
+      other: "",
+    },
+    images: [
+      {
+        src: "/assets/portfolio/gp-slovenia/gp-slovenia-3.jpg",
+        alt: "GP Slovenia",
+        width: 448,
+        height: 448,
+        size: "double",
+      },
+      {
+        src: "/assets/portfolio/gp-slovenia/gp-slovenia-2.jpg",
+        alt: "GP Slovenia",
+        width: 448,
+        height: 212,
+        size: "wide",
+      },
+      {
+        src: "/assets/portfolio/gp-slovenia/gp-slovenia-5.jpg",
+        alt: "GP Slovenia",
+        width: 212,
+        height: 212,
+        size: "normal",
+      },
+      {
+        src: "/assets/portfolio/gp-slovenia/gp-slovenia-6.jpg",
+        alt: "GP Slovenia",
+        width: 212,
+        height: 212,
+        size: "normal",
+      },
+    ],
+    relatedProjects: [],
+    excerpt: {
+      title: "GoPro Creator Summit",
+      subtitle: "Bled, Slovenia",
+      text: "Following the launch of the HERO12 camera. GoPro invited a select group of athletes, advocates, and leading content creators to Bled, Slovenia. The purpose of the summit was to give hands-on product training and take part in some extreme activities in one of the world’s most spectacular playgrounds.",
+      image: {
+        src: "/assets/portfolio/gp-slovenia/gp-slovenia-1.jpg",
+        alt: "GP Slovenia",
+        width: 448,
+        height: 448,
+      },
+    },
+  },
+  {
+    slug: "simscale",
+    featuredImage: {
+      src: "/assets/portfolio/simscale/simscale-1.jpg",
+      alt: "SimScale",
+      width: 920,
+      height: 448,
+    },
+
+    title: "SimScale",
+    subtitle: "Munich, Germany",
+    category: "branding",
+    paragraph:
+      "SimScale, established in Munich in 2012, is a pioneering engineering software startup. Its cloud-based platform revolutionizes the capabilities of engineers and designers globally by enabling robust Computational Fluid Dynamics (CFD), Finite Element Analysis (FEA), and thermal analysis. This groundbreaking technology dismantles numerous barriers that engineering firms encounter in realising their projects.",
+    details: {
+      date: "",
+      location: "Munich, Germany",
+      other: "",
+    },
+    images: [
+      {
+        src: "/assets/portfolio/simscale/simscale-3.jpg",
+        alt: "SimScale",
+        width: 448,
+        height: 448,
+        size: "double",
+      },
+      {
+        src: "/assets/portfolio/simscale/simscale-4.jpg",
+        alt: "SimScale",
+        width: 448,
+        height: 212,
+        size: "wide",
+      },
+      {
+        src: "/assets/portfolio/simscale/simscale-5.jpg",
+        alt: "SimScale",
+        width: 212,
+        height: 212,
+        size: "normal",
+      },
+      {
+        src: "/assets/portfolio/simscale/simscale-6.jpg",
+        alt: "SimScale",
+        width: 212,
+        height: 212,
+        size: "normal",
+      },
+    ],
+    relatedProjects: [],
+    excerpt: {
+      title: "SimScale",
+      subtitle: "Munich, Germany",
+      text: "SimScale, established in Munich in 2012, is a pioneering engineering software startup. Its cloud-based platform revolutionizes the capabilities of engineers and designers globally by enabling robust Computational Fluid Dynamics (CFD), Finite Element Analysis (FEA), and thermal analysis. This groundbreaking technology dismantles numerous barriers that engineering firms encounter in realising their projects.",
+      image: {
+        src: "/assets/portfolio/simscale/simscale-2.jpg",
+        alt: "SimScale",
         width: 448,
         height: 448,
       },
