5080c5da6897e4715bd83fe2cdcf94c4b150cb70
Aleksandar Timic <aleksandar.timic@meld.com>
2024-02-28 17:08:40 +0100

    Login page layout

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,21 @@
+import styles from "./login.module.scss";
+import Button from "@/components/button/Button.tsx";
+
 export default function Login() {
   return (
-    <>
-      <h1>Login</h1>
-    </>
+    <div className={styles.loginPage}>
+      <div className={styles.loginPageInner}>
+        <div className={styles.loginFormWrapper}>
+          <h1>Welcome</h1>
+          <h2>Login required</h2>
+          <div>
+            <form className={styles.loginForm}>
+              <input type={"password"} placeholder={"Password"} />
+              <Button id={"submit-login"} name={"Continue"} />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
